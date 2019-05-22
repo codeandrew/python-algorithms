89433b55be00e66780fb5ef8f1563c461bdf4896
Jean Andrew Fuentes <jeanandrewfuentes@gmail.com>
2019-05-22 09:10:23 +0800

    add persistent bugger

---
+++ persistent_bugger.py
@@ -0,0 +1,49 @@
+"""
+Write a function, persistence, that takes in a positive parameter num and returns its multiplicative persistence, which is the number of times you must multiply the digits in num until you reach a single digit.
+
+For example:
+
+ persistence(39) => 3  # Because 3*9 = 27, 2*7 = 14, 1*4=4
+                       # and 4 has only one digit.
+
+ persistence(999) => 4 # Because 9*9*9 = 729, 7*2*9 = 126,
+                       # 1*2*6 = 12, and finally 1*2 = 2.
+
+ persistence(4) => 0   # Because 4 is already a one-digit number.
+ persistence(39) # returns 3, because 3*9=27, 2*7=14, 1*4=4
+                 # and 4 has only one digit
+
+ persistence(999) # returns 4, because 9*9*9=729, 7*2*9=126,
+                  # 1*2*6=12, and finally 1*2=2
+
+ persistence(4) # returns 0, because 4 is already a one-digit number
+ """
+
+def persistence(n):
+    i = 0
+    def r(n, counter):
+        if len(str(n)) <= 1: return counter
+        s = reduce((lambda x, y: int(x) * int(y)), list(str(n)))
+        counter += 1
+        return r(s, counter)
+    return r(n, i)
+
+ """
+ other solutions
+ """"
+import operator
+def persistence(n):
+    i = 0
+    while n>=10:
+        n=reduce(operator.mul,[int(x) for x in str(n)],1)
+        i+=1
+    return i
+
+def persistence(n):
+    nums = [int(x) for x in str(n)]
+    sist = 0
+    while len(nums) > 1:
+        newNum = reduce(lambda x, y: x * y, nums)
+        nums = [int(x) for x in str(newNum)]
+        sist = sist + 1
+    return sist
