4f2213970b774ec1b37a30d497a82bc2d0c931be
Jean Andrew Fuentes <jeanandrewfuentes@gmail.com>
2022-10-19 21:23:31 +0800

    Create greet.py

---
+++ greet.py
@@ -0,0 +1,22 @@
+"""
+Create a function that gives a personalized greeting. This function takes two parameters: name and owner.
+
+Use conditionals to return the proper message:
+
+case	return
+name equals owner	'Hello boss'
+otherwise	'Hello guest'
+"""
+
+# MY SOLUTION
+def greet(name, owner):
+    return "Hello boss" if name == owner else "Hello guest"
+  
+"""
+Tags:
+
+one line if else
+shorthand if else
+
+
+"""
